6584b27f6317e2df06bfd1afbd76221327cab803
simedunn <76657534+simedunn@users.noreply.github.com>
2023-04-08 15:03:59 -0400

    Create private_key_decrypt.py

---
+++ scripts/private_key_decrypt.py
@@ -0,0 +1,10 @@
+decrypted_data = private_key.decrypt(
+    encrypted_data,
+    padding.OAEP(
+        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+        algorithm=hashes.SHA256(),
+        label=None
+    )
+)
+
+print(decrypted_data)  # should output "b'Hello, world!'"
