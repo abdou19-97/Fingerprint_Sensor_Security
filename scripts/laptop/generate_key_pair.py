65748426e0c65ad923a8d7e169f0a41e413d9751
abdou19-97 <61571968+abdou19-97@users.noreply.github.com>
2023-04-09 16:49:51 -0400

    Update generate_key_pair.py

--- scripts/laptop/generate_key_pair.py
+++ scripts/laptop/generate_key_pair.py
@@ -16,3 +16,52 @@ with open('public_key.pem', 'wb') as f:
         encoding=serialization.Encoding.PEM,
         format=serialization.PublicFormat.SubjectPublicKeyInfo
     ))
+
+    
+    
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes
+
+# Generate a public/private key pair
+private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048,
+)
+public_key = private_key.public_key()
+
+# Alice encrypts a message using Bob's public key
+message = b"Hello, Bob!"
+# The code shows how to configure padding for encryption
+#  or decryption using the OAEP (Optimal Asymmetric Encryption Padding) scheme.
+ciphertext = public_key.encrypt(
+    message,
+    padding.OAEP( # The padding.OAEP method initializes a new instance of the OAEP padding class.
+        # mgf=padding.MGF1(algorithm=hashes.SHA256()) 
+        # specifies the Mask Generation Function (MGF1), which is used to randomize the input before encryption.
+        # It is set to MGF1 with SHA-256 hash function.
+        mgf=padding.MGF1(algorithm=hashes.SHA256()), 
+        algorithm=hashes.SHA256(), # algorithm=hashes.SHA256() specifies the hash function used in the OAEP scheme.
+        label=None,
+    )
+   
+)
+
+
+# Bob decrypts the message using his private key
+plaintext = private_key.decrypt(
+    ciphertext,
+    padding.OAEP(
+        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+        algorithm=hashes.SHA256(),
+        label=None,
+    )
+)
+
+print("Original message:", message)
+print("Ciphertext message:",ciphertext)
+print("Decrypted message:", plaintext)
+
+
+# In summary, these lines of code set up an OAEP padding scheme that uses SHA-256 for both the MGF1 and OAEP hash functions and doesn't use any label.
+# This padding scheme can be used to encrypt or decrypt messages.
